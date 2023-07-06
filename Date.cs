2aaef4503de7c4930f4f84e6a6195aa1bba952e8
unknown <narminna@code.edu.az>
2023-07-06 17:27:59 +0400

    DateTime

---
+++ Date.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string date;
+            Console.WriteLine("Enter date: ");
+            date = Console.ReadLine();
+            try
+            {
+                DateTime dt = DateTime.ParseExact(date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                Console.WriteLine(dt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
